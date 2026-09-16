293c7445ad8c8ac1c7fc51b7acdc0f0b78964655
shreyaskn72 <60369972+shreyaskn72@users.noreply.github.com>
2026-09-16 06:19:16 +0000

    flask api

--- app/main.py
+++ app/main.py
@@ -1,1 +1,11 @@
-print("Hello from the Dev Container!")
+from flask import Flask, jsonify
+
+app = Flask(__name__)
+
+@app.route('/api/hello', methods=['GET'])
+def hello_world():
+    # jsonify automatically format the dictionary as a JSON response
+    return jsonify({"message": "Hello from Shreyas"})
+
+if __name__ == '__main__':
+    app.run(debug=True)
